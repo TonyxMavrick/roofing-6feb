23d0af1d11438886f0606b50db1b906bb1e68043
TonyxMavrick <drivingtrainingschoolmohancar@gmail.com>
2026-02-06 00:34:58 -0800

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,18 @@
     <title>PrimeShield Roofing - Trusted Roofing Repair Experts Across the USA</title>
     <meta name="description" content="Fast, reliable, and affordable professional roof repair services for homes and businesses across the United States. 24/7 emergency services available.">
     <link rel="stylesheet" href="styles.css">
+    !-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,5006829,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5006829&101" alt="website hit counter" border="0"></a></noscript>
+<!-- Histats.com  END  -->
 </head>
 <body>
     <!-- Top Bar -->
